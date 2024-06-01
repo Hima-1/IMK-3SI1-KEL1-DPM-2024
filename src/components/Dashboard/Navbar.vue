1d577a437db2fe6f7a2d89c49fdda7eb8a80d7e1
Himawan Wahid Ikhwansyah <himawanwahid12@gmail.com>
2024-06-01 11:57:16 +0700

    arsip dan anggaran

--- src/components/Dashboard/Navbar.vue
+++ src/components/Dashboard/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav :class="['w-full h-[92px] flex items-center justify-between bg-[#1A5796] drop-shadow-md p-4', { 'fixed': isScrolled, 'top-0': isScrolled, 'w-full': isScrolled }]">
-    <div class="flex items-center space-x-4 ml-16">
-      <img src="../../assets/images/logo_dpm.webp" alt="Logo" class="w-[62px] h-[62px] flex-shrink-0">
-      <span class="text-white font-poppins text-2xl font-bold">SIASPIMAN</span>
-    </div>
+  <nav :class="['w-full h-[5.75rem] flex items-center justify-between bg-[#1A5796] drop-shadow-md p-4', { 'fixed': isScrolled, 'top-0': isScrolled, 'w-full': isScrolled }]">
+    <router-link to="/">
+      <div class="flex items-center space-x-4 ml-16">
+        <img src="../../assets/images/logo_dpm.webp" alt="Logo" class="w-[3.875rem] h-[3.875rem] flex-shrink-0">
+        <span class="text-white font-poppins text-2xl font-bold">SIASPIMAN</span>
+      </div>
+    </router-link>
     <div class="flex items-center space-x-8 mr-16">
       <router-link to="/dashboard" exact class="nav-link" :exact-active-class="'active-link'">
         <span class="text-[#EBF3F5] text-center font-poppins text-lg">Aspirasi</span>
@@ -15,16 +17,15 @@
         <span class="text-[#EBF3F5] text-center font-poppins text-lg">Anggaran Imapolstat</span>
       </router-link>
       <div class="flex items-center space-x-2">
-        <img src="../../assets/icon/account.svg" alt="User Photo" class="w-[60px] h-[60px] flex-shrink-0">
+        <img src="../../assets/icon/account.svg" alt="User Photo" class="w-[3.75rem] h-[3.75rem] flex-shrink-0">
         <div class="text-right">
           <span class="text-[#EBF3F5] font-poppins text-lg font-medium">Nama User</span>
         </div>
-        <img src="../../assets/icon/arrow-down.svg" alt="Dropdown Icon" class="w-[11px] h-[9px] fill-current text-[#EBF3F5]">
+        <img src="../../assets/icon/arrow-down.svg" alt="Dropdown Icon" class="w-[0.6875rem] h-[0.5625rem] fill-current text-[#EBF3F5]">
       </div>
     </div>
   </nav>
 </template>
-
 
 <script>
 export default {
@@ -47,7 +48,6 @@ export default {
   }
 }
 </script>
-
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700;800&display=swap');
