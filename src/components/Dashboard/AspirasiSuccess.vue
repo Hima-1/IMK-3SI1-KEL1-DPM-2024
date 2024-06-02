18c70fa6621b29857f35eb118c4da6fb0a4355c8
Himawan Wahid Ikhwansyah <himawanwahid12@gmail.com>
2024-06-02 23:32:03 +0700

    Aspirasi

---
+++ src/components/Dashboard/AspirasiSuccess.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div class="modal-content">
+      <img
+          src="../../assets/images/success.webp"
+          alt="Gambar Sukses"
+          class="modal-image"
+      />
+      <h1 class="sukses-text">SUKSES >_<</h1>
+      <p class="sambatan-text">Sambatan kamu telah dikirim</p>
+      <div class="modal-buttons">
+        <button @click="confirm" class="confirm-button">Oke</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    confirm() {
+      this.$emit('confirm');
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+
+.modal-content {
+  width: 90%;
+  max-width: 27.75rem; /* 444px/16 */
+  height: 31.6875rem; /* 507px/16 */
+  background-color: #ebf3f5;
+  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
+  border-radius: 1.25rem; /* 20px */
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-image {
+  width: 17.9038rem; /* 286.461px/16 */
+  height: 13.0624rem; /* 208.999px/16 */
+  flex-shrink: 0;
+}
+
+.sukses-text {
+  width: 20.25rem; /* 324px/16 */
+  color: #1a5796;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.5625rem; /* 25px/16 */
+  font-weight: 600;
+  line-height: 2.3425rem; /* 37.47px/16 */
+  margin-top: 1rem;
+}
+
+.sambatan-text {
+  width: 21.4375rem; /* 343px/16 */
+  color: #1a5796;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.25rem; /* 20px/16 */
+  font-weight: 400;
+  line-height: 1.8735rem; /* 29.976px/16 */
+  margin-top: 1rem;
+}
+
+.modal-buttons {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 1.5rem;
+}
+
+.confirm-button {
+  width: 9.5625rem; /* 153px/16 */
+  height: 3.5848rem; /* 57.357px/16 */
+  border-radius: 9999px;
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.25rem; /* 20px/16 */
+  font-weight: 700;
+  text-align: center;
+  line-height: 3.5848rem; /* 57.357px/16 */
+  background-color: #1a5796;
+  color: #ebf3f5;
+  border: none;
+}
+</style>
--- src/views/Aspirasi.vue
+++ src/views/Aspirasi.vue
@@ -1,11 +1,68 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="min-h-screen flex items-center justify-center bg-[#EBF3F5] pt-0">
+    <div class="w-[90vw] max-w-[1661px] h-[989px] bg-[#EBF3F5] rounded-lg p-8 pt-0">
+      <HeaderComponent />
+      <div class="px-4">
+        <hr class="w-full border-t-2 border-[#C8D6DF] mb-8">
+      </div>
+      <div class="flex items-center mb-8">
+        <img src="@/assets/icon/megaphone.svg" alt="Megaphone Icon" class="w-[4vw] h-[4vw] max-w-[58px] max-h-[58px] mr-4">
+        <p class="text-[#1A5796] font-poppins text-[2.5vw] max-text-[40px] font-normal leading-normal">Aspirasi/Sambatan</p>
+      </div>
+      <div class="flex space-x-8">
+        <IntroComponent />
+        <FormComponent @show-confirmation-popup="showConfirmationPopup" />
+      </div>
+    </div>
+    <AspirasiConfirmationPopup v-if="isConfirmationPopupVisible" @confirm="handleConfirmation" @cancel="hideConfirmationPopup" />
+    <AspirasiSuccess v-if="isSuccessPopupVisible" @confirm="hideSuccessPopup" />
+  </div>
 </template>
 
-<style scoped>
+<script>
+import HeaderComponent from '@/components/Dashboard/AspirasiHeader.vue'
+import IntroComponent from '@/components/Dashboard/AspirasiIntro.vue'
+import FormComponent from '@/components/Dashboard/AspirasiForm.vue'
+import AspirasiConfirmationPopup from "@/components/Dashboard/AspirasiConfirmationPopup.vue";
+import AspirasiSuccess from "@/components/Dashboard/AspirasiSuccess.vue";
 
+export default {
+  name: 'AspirasiIntro',
+  components: {
+    AspirasiSuccess,
+    HeaderComponent,
+    IntroComponent,
+    FormComponent,
+    AspirasiConfirmationPopup,
+  },
+  data() {
+    return {
+      isConfirmationPopupVisible: false,
+      isSuccessPopupVisible: false,
+    };
+  },
+  methods: {
+    showConfirmationPopup() {
+      this.isConfirmationPopupVisible = true;
+    },
+    hideConfirmationPopup() {
+      this.isConfirmationPopupVisible = false;
+    },
+    handleConfirmation() {
+      this.isConfirmationPopupVisible = false;
+      this.isSuccessPopupVisible = true;
+    },
+    hideSuccessPopup() {
+      this.isSuccessPopupVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap');
+
+.font-poppins {
+  font-family: 'Poppins', sans-serif;
+}
 </style>
