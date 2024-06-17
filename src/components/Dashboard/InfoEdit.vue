3428813ad13b443a0ab737fa4e4039a506ae1f78
whyklae <rista565@gmail.com>
2024-06-17 10:06:44 +0700

    EditProfile Responsive
    Menyediakan beberapa susunan layout profil dan text input

--- src/components/Dashboard/InfoEdit.vue
+++ src/components/Dashboard/InfoEdit.vue
@@ -8,36 +8,66 @@
       </div>
 
       <!-- Informasi Teks User -->
-      <div class="grid grid-cols-3">
-        <div class="grid col-span-2 text-[18px]">
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">Email</p>
-            <input type="text" placeholder="You can't touch this" class="col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" disabled />
+      <div class="grid lg:grid-cols-3 lg:max-xl:gap-x-6 xl:gap-x-2">
+        <!-- Foto Device Small -->
+        <div class="grid lg:hidden justify-center">
+          <div class="relative w-fit mb-6">
+            <img src="../../assets/icon/account.svg" alt="User Photo" class="border-4 border-[#6A6C7A] rounded-full w-[200px] md:w-[250px] h-[200px] md:h-[250px] object-center object-cover" />
+            <div class="absolute bottom-3 right-5 bg-[#1A5796] rounded-full p-2 w-fit">
+              <img src="../../assets/icon/camera.svg" alt="User Photo" class="" />
+            </div>
+          </div>
+        </div>
+
+        <div class="grid lg:col-span-2 gap-y-3 text-[18px]">
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Email</p>
+            <input
+              type="text"
+              placeholder="You can't touch this"
+              class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl"
+              disabled
+            />
           </div>
 
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">Nama</p>
-            <input type="text" placeholder="You can't touch this" class="col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" disabled />
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Nama</p>
+            <input
+              type="text"
+              placeholder="You can't touch this"
+              class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl"
+              disabled
+            />
           </div>
 
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">NIM</p>
-            <input type="text" placeholder="You can't touch this" class="col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" disabled />
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">NIM</p>
+            <input
+              type="text"
+              placeholder="You can't touch this"
+              class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl"
+              disabled
+            />
           </div>
 
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">Kelas</p>
-            <input type="text" placeholder="You can't touch this" class="col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" disabled />
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Kelas</p>
+            <input
+              type="text"
+              placeholder="You can't touch this"
+              class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 input-sm text-[17px] py-5 bg-[#C8D6DF] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl"
+              disabled
+            />
           </div>
 
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">No. HP</p>
-            <input type="text" placeholder="Type here" class="col-span-4 input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">No. HP</p>
+            <input type="text" placeholder="Type here" class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
           </div>
 
-          <div class="grid grid-cols-5 items-center">
-            <p class="font-bold">Provider</p>
-            <select class="col-span-4 select select-bordered select-md text-[17px] bg-[#F6F6F6] border-2 border-[#6A6C7A] w-full max-w-xl">
+          <div class="grid min-[425px]:grid-cols-8 lg:grid-cols-5 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Provider</p>
+            <select class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-4 select select-bordered select-md text-[17px] bg-[#F6F6F6] border-2 border-[#6A6C7A] w-full max-w-xl">
               <option disabled selected>Pilih Provider</option>
               <option v-for="provider in providers">{{ provider }}</option>
             </select>
@@ -45,14 +75,17 @@
         </div>
 
         <!-- Foto User -->
-        <div class="grid grid-cols-3 gap-4 text-[18px] items-center">
-          <div class="col-span-2 col-start-2 col-end-4">
-            <img src="../../assets/icon/account.svg" alt="User Photo" class="border-2 border-[#6A6C7A] p-2 w-[300px] object-center object-contain" />
+        <div class="hidden lg:grid grid-cols-4 gap-y-5 text-[18px] items-center">
+          <div class="col-span-3 lg:max-xl:col-span-4 col-start-2 grid justify-center lg:relative lg:max-xl:mb-4">
+            <img src="../../assets/icon/account.svg" alt="User Photo" class="border-2 lg:max-xl:border-4 lg:rounded-full xl:rounded-none border-[#6A6C7A] lg:p-0 xl:p-2 w-[300px] object-center object-cover" />
+            <div class="hidden lg:max-xl:block absolute bottom-3 right-5 bg-[#1A5796] rounded-full p-2 w-fit">
+              <img src="../../assets/icon/camera.svg" alt="User Photo" class="" />
+            </div>
           </div>
 
-          <div class="font-bold">Foto Profil</div>
-          <div class="col-span-2">
-            <input type="file" class="file-input file-input-md file-input-bordered w-full file:bg-[#C8D6DF] file:border-none file:text-[#6A6C7A] bg-[#F6F6F6] border-2 border-[#6A6C7A]" />
+          <div class="grid col-span-4 items-center">
+            <div class="hidden xl:grid col-span-1 font-bold pr-3">Foto Profil</div>
+            <input type="file" class="col-span-3 col-start-2 grid file-input file-input-md file-input-bordered w-full file:bg-[#C8D6DF] file:border-none file:text-[#6A6C7A] bg-[#F6F6F6] border-2 border-[#6A6C7A]" />
           </div>
         </div>
       </div>
--- src/components/Dashboard/InfoRekening.vue
+++ src/components/Dashboard/InfoRekening.vue
@@ -2,17 +2,17 @@
   <div class="min-h-fit">
     <div class="py-6">
       <!-- Judul dan Deskripsi -->
-      <div class="grid grid-rows-2 gap-y-2 my-4">
-        <h1 class="text-[#1A5796] font-poppins text-4xl">Informasi Rekening</h1>
-        <p class="text-[#2F7DC1]">Pastikan isian rekening sudah benar, mencakup nomor rekening, nama bank yang digunakan, dan atas nama yang sesuai. Rekening mahasiswa harus atas nama pribadi dan pastikan rekening masih aktif</p>
+      <div class="my-4">
+        <h1 class="text-[#1A5796] font-poppins text-xl lg:text-4xl">Informasi Rekening</h1>
+        <p class="text-[#2F7DC1] my-4 text-[15px] lg:text-[23px]">Pastikan isian rekening sudah benar, mencakup nomor rekening, nama bank yang digunakan, dan atas nama yang sesuai. Rekening mahasiswa harus atas nama pribadi dan pastikan rekening masih aktif</p>
       </div>
 
       <div>
-        <div class="grid grid-cols-5 gap-x-5 text-[#6A6C7A]">
+        <div class="grid grid-cols-1 lg:grid-cols-8 gap-y-3 lg:gap-y-0 lg:gap-x-5 text-[#6A6C7A]">
           <!-- Bank -->
-          <div class="grid grid-cols-3 items-center">
-            <p class="font-bold">Bank</p>
-            <select v-model="selectedOption" class="col-span-2 select select-bordered select-md text-[17px] bg-[#F6F6F6] border-2 border-[#6A6C7A] w-full max-w-xl">
+          <div class="col-span-3 lg:col-span-2 grid min-[425px]:grid-cols-8 lg:grid-cols-3 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Bank</p>
+            <select v-model="selectedOption" class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-2 select select-bordered select-md text-[17px] bg-[#F6F6F6] border-2 border-[#6A6C7A] w-full max-w-xl">
               <option disabled selected value="">Pilih Bank</option>
               <option value="BRI">BRI</option>
               <option value="Mandiri">Mandiri</option>
@@ -24,15 +24,15 @@
           </div>
 
           <!-- Nomor Rekening -->
-          <div class="col-span-2 grid grid-cols-3 items-center">
-            <p class="font-bold">Nomor rekening</p>
-            <input type="text" placeholder="Type here" class="col-span-2 input input-bordered input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
+          <div class="col-span-3 grid min-[425px]:grid-cols-8 lg:grid-cols-3 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 text-wrap xl:text-nowrap font-bold">Nomor rekening</p>
+            <input type="text" placeholder="Type here" class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-2 input input-bordered input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
           </div>
 
           <!-- Atas Nama -->
-          <div class="col-span-2 grid grid-cols-4 items-center">
-            <p class="font-bold">Atas Nama</p>
-            <input type="text" placeholder="Type here" class="col-span-3 input input-bordered input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
+          <div class="col-span-3 grid min-[425px]:grid-cols-8 lg:grid-cols-4 items-center">
+            <p class="min-[425px]:col-span-2 lg:col-span-1 font-bold">Atas Nama</p>
+            <input type="text" placeholder="Type here" class="min-[425px]:col-start-3 min-[425px]:col-end-9 lg:col-span-3 input input-bordered input-sm text-[17px] py-5 bg-[#F6F6F6] border-2 border-[#6A6C7A] px-2 rounded-lg w-full max-w-xl" />
           </div>
         </div>
       </div>
