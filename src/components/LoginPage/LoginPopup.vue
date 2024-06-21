be2efeee345f761cd108a7d69352ade3c563739b
irsyadfa <irsyadfadhila17@gmail.com>
2024-06-21 16:52:54 +0700

    update login popup

--- src/components/LoginPage/LoginPopup.vue
+++ src/components/LoginPage/LoginPopup.vue
@@ -2,7 +2,7 @@
   <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 p-4">
     <div class="w-full max-w-[600px] h-[475px] bg-[#EBF3F5] rounded-[27px] flex flex-col items-center p-6 relative">
       <img src="@/assets/icon/close.svg" @click="$emit('close')" class="absolute top-[20px] right-[28px] w-[40px] h-[40px] cursor-pointer" alt="Close Icon" />
-      <div class="w-full max-w-[625px] h-[576px] flex flex-col items-center justify-between mt-[12px] mb-[12px] px-[32px]">
+      <div class="w-full max-w-[625px] h-[576px] flex flex-col items-center justify-between mt-[12px] mb-[12px] lg:px-6">
         <img src="@/assets/images/logo_dpm.webp" class="w-[125px] h-auto flex-shrink-0 mb-8" alt="Logo DPM" />
 
         <div class="w-full h-[50px] flex items-center border-2 border-[#1A5796] bg-[#EBF3F5] rounded-[36px] shadow-inner mb-6 px-4">
@@ -15,7 +15,7 @@
           />
         </div>
 
-        <div class="w-full h-[50px] flex items-center border-2 border-[#1A5796] bg-[#EBF3F5] rounded-[36px] shadow-inner mb-6 px-6">
+        <div class="w-full h-[50px] flex items-center border-2 border-[#1A5796] bg-[#EBF3F5] rounded-[36px] shadow-inner mb-6 pl-6 pr-2">
           <img src="@/assets/icon/password.svg" class="w-[20px] h-[30px] flex-shrink-0" alt="Password Icon" />
           <input
               v-model="password"
@@ -27,12 +27,12 @@
         </div>
 
         <div @click="login" class="w-full h-[50px] flex items-center justify-center bg-[#1A5796] rounded-[36px] shadow-lg cursor-pointer mb-4">
-          <p class="text-[#EBF3F5] text-center font-poppins text-[20px] font-semibold">
+          <p class="text-[#EBF3F5] text-center font-poppins text-[16px] md:text-[18px] font-semibold">
             Login
           </p>
         </div>
 
-        <p @click="$emit('forgot-password')" class="text-[#1A5796] text-center font-poppins text-[15px] font-normal cursor-pointer">
+        <p @click="$emit('forgot-password')" class="text-[#1A5796] text-center font-poppins text-[13px] font-normal cursor-pointer">
           Lupa Password?
         </p>
       </div>
