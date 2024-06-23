df720373be3d360bf6dcc94fa34b97d6892ff35f
Himawan Wahid Ikhwansyah <himawanwahid12@gmail.com>
2024-06-23 10:41:27 +0700

    tes

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,56 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LandingLayout from "@/layouts/LandingLayout.vue";
-import DashboardLayout from "@/layouts/DashboardLayout.vue";
-import Home from "@/views/Home.vue";
-import Berita from "@/views/Berita.vue";
-import Selengkapnya from "@/views/Selengkapnya.vue";
-import Galeri from "@/views/Galeri.vue";
-import Struktur from "@/views/Struktur.vue";
-import LoginPage from "@/views/Login.vue";
-import Aspirasi from "@/views/Aspirasi.vue";
-import ArsipDokumen from "@/views/Arsip.vue";
-import AnggaranImpolstat from "@/views/Anggaran.vue";
-import DetailAnggaran from "@/views/DetailAnggaran.vue";
-import MyProfile from "@/views/MyProfile.vue";
-import EditProfile from "@/views/EditProfile.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import LandingLayout from '@/layouts/LandingLayout.vue';
+import DashboardLayout from '@/layouts/DashboardLayout.vue';
+import Home from '@/views/Home.vue';
+import Berita from '@/views/Berita.vue';
+import Selengkapnya from '@/views/Selengkapnya.vue';
+import Galeri from '@/views/Galeri.vue';
+import Struktur from '@/views/Struktur.vue';
+import LoginPage from '@/views/Login.vue';
+import Aspirasi from '@/views/Aspirasi.vue';
+import ArsipDokumen from '@/views/Arsip.vue';
+import AnggaranImpolstat from '@/views/Anggaran.vue';
+import DetailAnggaran from '@/views/DetailAnggaran.vue';
+import MyProfile from '@/views/MyProfile.vue';
+import EditProfile from '@/views/EditProfile.vue';
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     component: LandingLayout,
     children: [
-      { path: "", name: "Home", component: Home },
+      { path: '', name: 'Home', component: Home },
       {
-        path: "berita",
+        path: 'berita',
         children: [
-          { path: "", name: "Berita", component: Berita },
-          { path: "selengkapnya", name: "Selengkapnya", component: Selengkapnya },
+          { path: '', name: 'Berita', component: Berita },
+          { path: 'selengkapnya', name: 'Selengkapnya', component: Selengkapnya },
         ],
       },
-      { path: "galeri", name: "Galeri", component: Galeri },
-      { path: "struktur", name: "Struktur", component: Struktur },
+      { path: 'galeri', name: 'Galeri', component: Galeri },
+      { path: 'struktur', name: 'Struktur', component: Struktur },
     ],
   },
-  { path: "/login", name: "Login", component: LoginPage },
+  { path: '/login', name: 'Login', component: LoginPage },
   {
-    path: "/dashboard",
+    path: '/dashboard',
     component: DashboardLayout,
     children: [
-      { path: "", name: "Aspirasi", component: Aspirasi },
-      { path: "arsip-dokumen", name: "ArsipDokumen", component: ArsipDokumen },
+      { path: '', name: 'Aspirasi', component: Aspirasi },
+      { path: 'arsip-dokumen', name: 'ArsipDokumen', component: ArsipDokumen },
       {
-        path: "anggaran-impolstat",
-        name: "AnggaranImpolstat",
+        path: 'anggaran-impolstat',
+        name: 'AnggaranImpolstat',
         component: AnggaranImpolstat,
       },
       {
-        path: "anggaran-impolstat/:id",
-        name: "DetailAnggaran",
+        path: 'anggaran-impolstat/:id',
+        name: 'DetailAnggaran',
         component: DetailAnggaran,
         props: true,
       },
-      { path: "my-profile", name: "MyProfile", component: MyProfile },
-      { path: "edit-profile", name: "EditProfile", component: EditProfile },
+      { path: 'my-profile', name: 'MyProfile', component: MyProfile },
+      { path: 'edit-profile', name: 'EditProfile', component: EditProfile },
     ],
   },
 ];
